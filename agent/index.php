f9ede141d603bc8713cffbaace3f73354ceb6ddb
[OmG] Network <webmaster@omg-network.de>
2018-07-28 02:19:42 +0200

    Add files via upload

--- agent/index.php
+++ agent/index.php
@@ -8,12 +8,6 @@ if ($_GET['key'] != "AUTHKEY"){
     die("ERROR: Falscher Key !");
 }
 
-function steamCMD(){
-    shell_exec('mkdir /opt/steamcmd');
-    shell_exec('curl -sqL "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz" | tar zxvf - -C /opt/steamcmd > /dev/null &');
-
-}
-
 function liveExecuteCommand($cmd)
 {
 
@@ -52,40 +46,9 @@ switch ($_GET['GAME']){
     $gsvpass = $_GET['sv_password'];
     $grcon = $_GET['rcon_password'];
     $gtoken = $_GET['sv_setsteamaccount'];
-    // FastDL Settings
-    if (!isset($_GET['fastdl_user'])){
-        $fuser = "fastdl";
-    } else {
-        $fuser = $_GET['fastdl_user'];
-    }
-    if (!isset($_GET['fastdl_passwd'])){
-        $fpasswd = "fastdl";
-    } else {
-        $fpasswd = $_GET['fastdl_passwd'];
-    }
-    // Download Options
-    if (!isset($_GET['metamod'])){
-        $metamod = "https://mms.alliedmods.net/mmsdrop/1.10/mmsource-1.10.7-git961-linux.tar.gz";
-    } else {
-        $metamod = $_GET['metamod'];
-    }
-    if (!isset($_GET['sourcemod'])){
-        $sourcemod = "https://sm.alliedmods.net/smdrop/1.8/sourcemod-1.8.0-git6040-linux.tar.gz";
-    } else {
-        $sourcemod = $_GET['sourcemod'];
-    }
-    if (!isset($_GET['esl_cfg'])){
-        $esl_cfg = "http://fastdl.omg-network.de/csgo/esl.tar";
-    } else {
-        $esl_cfg = $_GET['esl_cfg'];
-    }
-    // Install Options
-    $ipv4 = $_GET['ip'];
-
+    
     // Start Download
-    steamCMD();
-    shell_exec('mkdir /opt/csgo');
-    liveExecuteCommand('/opt/steamcmd/steamcmd.sh +@sSteamCmdForcePlatformType linux +login anonymous +force_install_dir /opt/csgo +app_update 740 validate +quit');
+    liveExecuteCommand('install_csgo.sh -a '.$gtype.' -b '.$ghostname.' -c '.$gsvpass.' -d '.$grcon.' -e '.$gtoken.'');
 
 }
 
