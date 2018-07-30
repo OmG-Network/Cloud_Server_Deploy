ab21fbc6557c5707b9088f86aa4119e4323ca478
[OmG] Network <webmaster@omg-network.de>
2018-07-30 17:41:56 +0200

    Add files via upload

--- agent/index.php
+++ agent/index.php
@@ -8,36 +8,6 @@ if ($_POST['key'] != "AUTHKEY"){
     die("ERROR: Falscher Key !");
 }
 
-function liveExecuteCommand($cmd)
-{
-
-    while (@ ob_end_flush()); // end all output buffers if any
-
-    $proc = popen("$cmd 2>&1 ; echo Exit status : $?", 'r');
-
-    $live_output     = "";
-    $complete_output = "";
-
-    while (!feof($proc))
-    {
-        $live_output     = fread($proc, 4096);
-        $complete_output = $complete_output . $live_output;
-        echo "$live_output";
-        @ flush();
-    }
-
-    pclose($proc);
-
-    // POST exit status
-    preg_match('/[0-9]+$/', $complete_output, $matches);
-
-    // return exit status and intended output
-    return array (
-                    'exit_status'  => intval($matches[0]),
-                    'output'       => str_replace("Exit status : " . $matches[0], '', $complete_output)
-                 );
-}
-
 switch ($_POST['game']){
     case "CSGO":
     // Game Settings
@@ -48,7 +18,8 @@ switch ($_POST['game']){
     $gtoken = $_POST['sv_setsteamaccount'];
     
     // Start Download
-    liveExecuteCommand('/opt/install_csgo.sh -a '.$gtype.' -b '.$ghostname.' -c '.$gsvpass.' -d '.$grcon.' -e '.$gtoken.'');
+    exec('bash -c "exec nohup setsid /opt/install_csgo.sh -a '.$gtype.' -b '.$ghostname.' -c '.$gsvpass.' -d '.$grcon.' -e '.$gtoken.' > /dev/null 2>&1 &"');
+    echo "Installation Started";
 
 }
 
