930615178f71b7e008b356761601afecc06d7fb2
[OmG] Network <webmaster@omg-network.de>
2018-08-06 21:11:00 +0200

    Add files via upload

--- agent/index.php
+++ agent/index.php
@@ -8,6 +8,22 @@ if ($_POST['key'] != "AUTHKEY"){
     die("ERROR: Falscher Key !");
 }
 
+if(isset($_POST['status'])){
+    switch ($_POST['status']){
+        case "cpu":
+        $exec_loads = sys_getloadavg();
+        $exec_cores = trim(shell_exec("grep -P '^processor' /proc/cpuinfo|wc -l"));
+        echo round($exec_loads[1]/($exec_cores + 1)*100, 0) . '%';
+        exit();
+        
+        case "ram":
+        $exec_free = explode("\n", trim(shell_exec('free')));
+        $get_mem = preg_split("/[\s]+/", $exec_free[1]);
+        echo number_format(round($get_mem[2]/1024/1024, 2), 2) . '/' . number_format(round($get_mem[1]/1024/1024, 2), 2);
+        exit();
+    }
+}
+
 switch ($_POST['game']){
     case "csgo":
     // Game Settings
